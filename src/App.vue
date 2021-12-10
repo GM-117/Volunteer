f93978e4ea3d94cb4b2977ca5f219c30e6195057
Lisha.huang <435576787@qq.com>
2021-12-10 09:45:40 +0800

    chore：合并代码

--- src/App.vue
+++ src/App.vue
@@ -5,40 +5,39 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-import apis from '@/apis'
+import { mapActions } from "vuex";
+import apis from "@/apis";
 
 export default {
-  name: 'app',
+  name: "app",
   methods: {
     ...mapActions({
-      setConfig: 'setConfig',
-      setDict: 'setDict',
+      setConfig: "setConfig",
+      setDict: "setDict",
     }),
   },
   mounted() {
     this.$http.post(apis.OptionConfig_GetConfig).then((res) => {
-      this.setConfig(res.data.data)
-    })
+      this.setConfig(res.data.data);
+    });
   },
-}
+};
 </script>
 
 <style>
-@import "styles/app.css";
-/* #app {
+#app {
   margin: 0;
   border: 0;
   padding: 0;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
-body .el-table th.gutter{
-  display: table-cell!important;
+body .el-table th.gutter {
+  display: table-cell !important;
 }
 
-body .el-table colgroup.gutter{
-  display: table-cell!important;
-} */
+body .el-table colgroup.gutter {
+  display: table-cell !important;
+}
 </style>
